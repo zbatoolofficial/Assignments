2b8eae94d12f8e03f2468e363fcee8b126dee7ab
zara <zbatoolofficial@gmail.com>
2022-05-13 09:54:32 +0500

    update

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,38 @@
-var a=2;
-var b=7;
-document.write(a+b*a-b);
+// var a=2;
+// var b=7;
+// document.write(a+b*a-b);
+// 
+function add(){
+    let a=6; 
+    let b=3;
+    let z=a+b
+    console.log(z)
+}
+function sub(){
+    let a=6;
+    let b=8;
+    let z=a+b-a;
+    document.write(z);
+}
+// 
+const f = "Hello javascript";
+let g = 34
+let c = true
+let d = ["(hello , yes)"];
+let e = {
+    firstname: ("Demo"),
+}
+document.write(d);
+document.write(typeof d);
+console.log(d);
+console.log(typeof c);
+// 
+let q=3
+let w=3
+if(q==0){
+    alert('true');
+}else if(w==5){
+    alert('ok');
+}else {
+    alert('false');
+}
