1c047271ec305295f3472b6896239a92cddad77a
zara <zbatoolofficial@gmail.com>
2022-05-18 08:51:36 +0500

    update

--- javascript/java2.js
+++ javascript/java2.js
@@ -1,2 +1,30 @@
 
+// 
+  let Table = prompt("Enter Table Number");
+  Table = parseInt(Table);
 
+  for (let a = 1; 11 > a; a++) {
+      document.write(Table +"x"+ a+"="+Table * a+ "<br>");
+  }
+//   
+var totalrows =20;
+var output ="";
+for( var i = 1; i <= totalrows; i++){
+    for (var j = 1; j <= i; j++){
+        output += j + " ";
+    }
+    document.write(output);
+    output ="<br>";
+}
+// 
+let value = ['HTML', 'CSS', 'Bootstrap', 'JAVASCRIPT', 'PHP'];
+// document.write(value[2]);
+// document.write(value.length);
+value.forEach(element => {
+    document.write("<br>",element);
+    console.log("<br>",element);
+});
+// 
+value.forEach(function (x) {
+    document.write(x + "<br>");
+});
