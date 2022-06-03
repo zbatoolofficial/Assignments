7a362ddd048c130aaaef7e7f5c995c31c335f42d
zara <zbatoolofficial@gmail.com>
2022-06-03 10:49:07 +0500

    update

---
+++ Front-end/ass.js
@@ -0,0 +1,7 @@
+function myFunction(imgs) {
+   let expandImg = document.getElementById("expandedImg");
+   let imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
